5c8fa6b4afd2a26703790e793151a81a06fede27
hughfdjackson <hughfdjackson@googlemail.com>
2011-10-08 14:25:38 +0100

    added some skeleton functions for the API (as I imagine it will be), and thankouts in the README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Clash
 
 Clash is (intended to be) a lightweight micro-library supporting basic 2d collision detection. [under development]
+
+
+### Thanks to:
+
+Rob Evans, creator of [@isogenicengine](http://twitter.com/#!/IsogenicEngine), for permission to use the point-to-polygon collision detection algorithm [posted in his blog](http://www.isogenicengine.com/2010/10/13/spotlight-detecting-polygon-collision-in-javascript/)
+
+
+[GameJS](http://gamejs.org/) for being awesome enough to be licensed under MIT, allowing for reuse of a small snippet of code.  
--- src/clash.js
+++ src/clash.js
@@ -1,6 +1,22 @@
 !function (context, undefined) {
    
-    var clash = {}
+    // functional checking methods.  returns true if objects collide
+    var check = {
+        point_point: function(){},
+        point_circle: function(){},
+        point_aabb: function(){},
+        point_poly: function(){}
+        circle_circle: function(){},
+        circle_aabb: function(){},
+        circle_poly: function(){},
+        aabb_aabb: function(){},
+        aabb_poly: function(){},
+        poly_poly: function(){},
+    },
+   
+    clash = {
+        check: check
+    }
 
     context["clash"] = clash
 }(this)
