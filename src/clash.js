df0859e1112ada62c102679580cdff22f7389759
hughfdjackson <hughfdjackson@googlemail.com>
2011-10-08 16:31:14 +0100

    starting to flesh out first specs and functional checks

--- src/clash.js
+++ src/clash.js
@@ -1,21 +1,44 @@
 !function (context, undefined) {
-   
-    // functional checking methods.  returns true if objects collide
-    var check = {
-        point_point: function(){},
-        point_circle: function(){},
-        point_aabb: function(){},
-        point_poly: function(){}
-        circle_circle: function(){},
-        circle_aabb: function(){},
-        circle_poly: function(){},
-        aabb_aabb: function(){},
-        aabb_poly: function(){},
-        poly_poly: function(){},
-    },
-   
-    clash = {
-        check: check
+       
+    var clash = function(){
+
+        // functional checking methods.  returns true if objects collide.
+        // methods are eager - i.e. in cases of 'just' touching, will return yes
+        var check = {
+            point_point: function(A, B){
+                return ((A.x == B.x) && (A.y == B.y))
+            },
+            point_circle: function(A, B){            
+                var rsq = B.radius * B.radius,
+                    diffx = A.x - B.x,
+                    diffy = A.y - B.y,
+                    distsq = (diffx*diffx) + (diffy*diffy)
+                    
+                return distsq <= rsq
+            },
+            point_aabb: function(){},
+            point_poly: function(){},
+            circle_circle: function(A, B){
+                var rdistsq = (A.radius + B.radius) * (A.radius + B.radius),
+                    diffx = A.x - B.x,
+                    diffy = A.y - B.y,
+                    distsq = (diffx*diffx) + (diffy*diffy)
+                    
+                return distsq <= rdistsq
+                
+            },
+            circle_aabb: function(){},
+            circle_poly: function(){},
+            aabb_aabb: function(){},
+            aabb_poly: function(){},
+            poly_poly: function(){},
+        }
+
+        return {
+            check: check
+        }
+        
+
     }
 
     context["clash"] = clash
